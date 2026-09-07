dec666cddc663ca1ef64330378c36a76a8dc8d10
tamil <93432998+Tamil73388@users.noreply.github.com>
2026-09-07 13:55:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let start=document.queryselector(".level")
+let crnt=start
+
+let alert=0;
+while(crnt.parentElement){
+	alert++
+	crnt=start.parentElement	
+}
+alert(`The Level of the Element is: ${alert}`)
